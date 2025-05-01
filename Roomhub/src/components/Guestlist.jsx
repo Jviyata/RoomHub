00cb8c8441ecd01b813a3a775759f6e1e872f216
Jviyata <viyatarutasart@gmail.com>
2025-05-01 16:11:53 -0400

    Update Guestlist.jsx

--- Roomhub/src/components/Guestlist.jsx
+++ Roomhub/src/components/Guestlist.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react';
-import Navbar from ' ../components/Navbar';
+import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 const Guests = () => {
