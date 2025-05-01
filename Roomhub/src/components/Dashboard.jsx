0be5a6b27a39ca3df5cb30ffcc8546cb80307f98
Jviyata <viyatarutasart@gmail.com>
2025-05-01 16:11:27 -0400

    Update Dashboard.jsx

--- Roomhub/src/components/Dashboard.jsx
+++ Roomhub/src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useAppContext } from '../context/AppContext';
-import Navbar from ' ../components/Navbar';
+import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 const Dashboard = () => {
